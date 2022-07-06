acfdeb3d708b5887a22deea1b6c0b722de953319
webferrol <xurxo@webferrol.com>
2022-07-06 23:03:54 +0200

    Añadido spinner en ProfilesPage en el slider

--- src/pages/ProfilePage.vue
+++ src/pages/ProfilePage.vue
@@ -1,28 +1,60 @@
 <template>
     <div class="q-pa-md">
-        <q-carousel swipeable animated arrows v-model="slide" v-model:fullscreen="fullscreen" infinite>
-            <q-carousel-slide v-for="file of files" :key="file.index" :name="file.index" :img-src="file.url" />
+        <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+            <div v-show="showSimulatedReturnData">
 
-            <template v-slot:control>
-                <q-carousel-control position="bottom-right" :offset="[18, 18]">
-                    <q-btn push round dense color="white" text-color="primary"
-                        :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'" @click="fullscreen = !fullscreen" />
-                </q-carousel-control>
-            </template>
-        </q-carousel>
+
+                <q-carousel swipeable animated arrows v-model="slide" v-model:fullscreen="fullscreen" infinite>
+                    <q-carousel-slide v-for="file of files" :key="file.index" :name="file.index" :img-src="file.url" />
+                    <template v-slot:control>
+                        <q-carousel-control position="bottom-right" :offset="[18, 18]">
+                            <q-btn push round dense color="white" text-color="primary"
+                                :icon="fullscreen ? 'fullscreen_exit' : 'fullscreen'"
+                                @click="fullscreen = !fullscreen" />
+                        </q-carousel-control>
+                    </template>
+                </q-carousel>
+            </div>
+        </transition>
+        <q-inner-loading :showing="visible">
+            <q-spinner-gears size="xl" color="primary" />
+        </q-inner-loading>
     </div>
 </template>
 
 <script setup>
+import { useQuasar } from 'quasar';
 import { listAllUrls } from '../storage';
 import { ref } from 'vue'
 const slide = ref(1);
 const fullscreen = ref(false)
 const files = ref([]);
+const showSimulatedReturnData = ref(true);
+const visible = ref(false);
+
+const $q = useQuasar();
 
 
 (async () => {
-    files.value = await listAllUrls('proyectos');
+    try {
+        showSimulatedReturnData.value = false;
+        visible.value = true;
+        files.value = await listAllUrls('proyectos');
+        if (!files.value.length)
+            throw new Error("No se encontraron imágenes del Proyecto")
+    } catch (error) {
+        $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: `${error.message}`
+        })
+        console.log("Error--->", error)
+    } finally {
+        showSimulatedReturnData.value = true;
+        visible.value = false;
+    }
+
     //console.log(files.value)
 })();
 </script>
