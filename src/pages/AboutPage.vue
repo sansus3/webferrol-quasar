502dfb1aff2ccf6486e3be4f96ab4384654dfaee
webferrol <xurxo@webferrol.com>
2022-07-25 22:39:12 +0200

    Añadido ajax bar de quasar

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -3,19 +3,26 @@
         <h1 class="text-h4 text-right q-mr-lg">Acerca de</h1>
         <q-pagination v-if="max" input v-model="$current" :max="max" :boundary-links="false" direction-links />
         <div class="q-pa-md row items-start q-gutter-md">
-            <q-card v-for="item of datos?.data" :key="item.id" dark bordered class="bg-grey-9 my-card">
-                <q-card-section>
+            <q-card v-for="item of datos?.data" :key="item.id" bordered>
+                <q-card-section class="bg-primary text-white">
                     <div class="text-h6">{{ item.title }}</div>
                     <div class="text-subtitle2">{{ getDayMonthFullYear(item.dateEnd) }}</div>
                 </q-card-section>
 
-                <q-separator dark inset />
-
                 <q-card-section>
-                    <pre>{{ datos }}</pre>
+                    <q-list>
+                        <q-item-section>
+                            <q-item-label>{{ item.title }}</q-item-label>
+                            <q-item-label>{{ item.place }}</q-item-label>
+                            <q-item-label>{{ item.jobTitle }}</q-item-label>
+                            <q-item-label>{{ item.code }}</q-item-label>
+                        </q-item-section>
+                    </q-list>
+                    <!-- <pre>{{ datos }}</pre> -->
                 </q-card-section>
             </q-card>
         </div>
+        <q-ajax-bar ref="bar" position="bottom" color="accent" size="10px" skip-hijack />
     </q-page>
 </template>
 <script setup>
@@ -24,10 +31,11 @@ import { useDB } from '../cloud.firestore';
 import { getDayMonthFullYear } from '../functions'
 const db = useDB('workExperience');
 const { initPage, nextPage, previousPage, totalPages } = db;
+const bar = ref(null);
 const datos = ref(null);
 const max = ref(0);//Número de páginas máximas que hay
 const $current = ref(0);//página en la que nos encontramos actualmente
-const perPage = 2;
+const perPage = 4;
 
 //Datos imprescindibles al cargar la página
 const start = async () => {
@@ -45,6 +53,8 @@ start()
 // Ejecuta la función cuando cambia la variable reactiva $current.
 // La función callback puede obtener el valor actual (current) y anterior (prev).
 watch($current, async (current, prev) => {
+    const barRef = bar.value;
+    barRef.start()
     //console.log(`watch => current: ${current} prev: ${prev}`);
     if (current === 1) {
         datos.value = await initPage('dateEnd', perPage);
@@ -53,6 +63,6 @@ watch($current, async (current, prev) => {
         datos.value = await nextPage('dateEnd', datos.value.last, perPage);
     else
         datos.value = await previousPage('dateEnd', datos.value.last, perPage);
+    barRef.stop()
 });
-
 </script>
